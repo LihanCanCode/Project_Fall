9d1a8f65263a9575df2fd12dc738b4ebff6d0943
LihanCanCode <156469522+LihanCanCode@users.noreply.github.com>
2025-03-02 16:20:32 +0600

    Update map.h

--- src/map.h
+++ src/map.h
@@ -5,11 +5,14 @@
 
 class Map {
 public:
-    Map(int screenWidth, int screenHeight);
-    void Draw() const;
+    Texture2D background;
+    bool textureLoaded;
 
-private:
-    int screenWidth, screenHeight;
+    Map();
+    void Load(const char* filename);
+    void Draw(Vector2 cameraPosition);
+    void Unload();
+    ~Map();
 };
 
 #endif // MAP_H

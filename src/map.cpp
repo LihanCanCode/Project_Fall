f4bb59828ad2ae99ce49731821cd4afcc21778df
LihanCanCode <156469522+LihanCanCode@users.noreply.github.com>
2025-03-02 16:21:01 +0600

    Update map.cpp

--- src/map.cpp
+++ src/map.cpp
@@ -1,8 +1,43 @@
 #include "map.h"
 
-Map::Map(int screenWidth, int screenHeight)
-    : screenWidth(screenWidth), screenHeight(screenHeight) {}
+// Screen dimensions
+int screenWidth = 1000;
+int screenHeight = 950;
 
-void Map::Draw() const {
-    DrawRectangle(0, 0, screenWidth, screenHeight, DARKGREEN);
+Map::Map() {
+    textureLoaded = false;
+}
+
+void Map::Load(const char* filename) {
+    background = LoadTexture(filename);
+    if (background.id == 0) {
+        TraceLog(LOG_ERROR, "Failed to load background image!");
+        textureLoaded = false;
+    } else {
+        textureLoaded = true;
+        TraceLog(LOG_INFO, "Map background loaded successfully");
+    }
+}
+
+void Map::Draw(Vector2 cameraPosition) {
+    if (textureLoaded) {
+        DrawTexturePro(background,
+                       {0, 0, (float)background.width, (float)background.height},
+                       {0 - cameraPosition.x, 0 - cameraPosition.y, (float)background.width, (float)background.height},
+                       {0, 0}, 0, WHITE);
+    } else {
+        DrawRectangle(0, 0, screenWidth, screenHeight, LIGHTGRAY);
+        DrawText("Map texture not loaded", screenWidth / 2 - 100, screenHeight / 2, 20, DARKGRAY);
+    }
+}
+
+void Map::Unload() {
+    if (textureLoaded) {
+        UnloadTexture(background);
+        textureLoaded = false;
+    }
+}
+
+Map::~Map() {
+    Unload();
 }

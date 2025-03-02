1f47c67eb5bd77d778ac6190970233b4ed41dd1a
LihanCanCode <156469522+LihanCanCode@users.noreply.github.com>
2025-03-02 16:22:05 +0600

    Update game.h

--- src/game.h
+++ src/game.h
@@ -1,1 +1,25 @@
 
+#ifndef GAME_H
+#define GAME_H
+
+#include "player.h"
+#include "idle.h"
+#include "map.h"
+
+class Game {
+public:
+    Player player;
+    Idle idle;
+    Map map;
+    bool initialized;
+    bool showDebugInfo;
+    Vector2 cameraPosition;
+    Camera2D camera;
+
+    Game();
+    void Initialize();
+    void UpdateCamera();
+    void Run();
+};
+
+#endif // GAME_H

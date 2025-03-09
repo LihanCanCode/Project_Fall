72c6da1c1eaa88b53f3e26db550f3b7318c54bfd
LihanCanCode <156469522+LihanCanCode@users.noreply.github.com>
2025-03-09 14:29:36 +0600

    Update collision.h

--- src/collision.h
+++ src/collision.h
@@ -10,7 +10,10 @@ void ResolvePlayerKeyCollision(Rectangle& playerRect, Rectangle& keyRect, bool& 
 void ResolvePlayerHospitalCollision(Rectangle& playerRect, const Rectangle& hospitalRect, bool& insideHospital, bool& playerPositionUpdated);
 void ResolvePlayerCDSCollision(Rectangle& playerRect, const Rectangle& cdsRect, bool& insideCDS, bool& playerPositionUpdated);
 void ResolvePlayerLibraryCollision(Rectangle& playerRect, const Rectangle& libraryRect, bool& insideLibrary, bool& playerPositionUpdated);
+void ResolvePlayerClassroomCollision(Rectangle& playerRect, const Rectangle& libraryRect, bool& insideLibrary, bool& playerPositionUpdated);
 void InsideHospital(Rectangle& playerRect, const std::vector<Rectangle>& hospitalCollisions);
+void InsideLibrary(Rectangle& playerRect, const std::vector<Rectangle>& libraryCollisions);
 void ResolvePlayerBookCollision(Rectangle& playerRect, Rectangle& bookRect, bool& bookFound);
+
 
 #endif // COLLISION_H

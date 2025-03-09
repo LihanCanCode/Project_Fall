36fcfbe5888c8064038cb5b38f0bef3c4a858c67
LihanCanCode <156469522+LihanCanCode@users.noreply.github.com>
2025-03-09 14:32:17 +0600

    Update game.h

--- src/game.h
+++ src/game.h
@@ -24,15 +24,24 @@ public:
     Texture2D libraryTexture;
     Texture2D cdsTexture;
     Texture2D bookTexture;
+    Texture2D mysteryTexture;
+    Texture2D mysteryTexture2;
+    Texture2D classroomTexture;
+
+    Rectangle source1;
     bool keyFound;
     bool bookFound;
     bool keyVisible;
     Vector2 conversationPosition;
+    Vector2 mysteryPosition;
     bool firstCollisionOccurred;
     bool mazeActive;
     bool insideHospital;
     bool insideCDS;
+    bool insideClassroom;
     bool insideLibrary;
+    bool showMystery;
+    bool showMystery2;
 
 
     int conversationStep = 0;
